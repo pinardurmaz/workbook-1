78fdd9b71e7f39d22f403b5a5b01627210049793
Pinar Durmaz <pnr.drmz02@gmail.com>
2025-04-10 16:09:10 -0400

    Updated Payroll Calculator with overtime pay

---
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner salary = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = salary.nextLine();
+
+        System.out.print("Please enter your hours worked: ");
+        float hours = salary.nextFloat();
+
+        System.out.print("Please enter your pay rate: ");
+        float payRate = salary.nextFloat();
+
+        System.out.println("Employee Name: " + name + ", Pay Rate: " + payRate * hours);
+        System.out.printf("Employee Name: %s, Pay Rate: %2f" , name, payRate );
+    }
+}
